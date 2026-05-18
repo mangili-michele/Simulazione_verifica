df415b02069896cda308d62b4ae52e62f2107937
Mangili Michele <mangili.michele.studente@itispaleocapa.it>
2026-05-18 09:20:49 +0000

    si

--- codice.cpp
+++ codice.cpp
@@ -11,6 +11,68 @@ IN DN PN
 La prima riga contiene il numero N dei processi. Seguono N righe, per ciascuna delle quali è leggibile l'id del processo (I, primo numero), la durata (D, secondo numero) e la priorità (P, terzo numero). Il processo a priorità più alta va eseguito per primo, e a parità di priorità si prende il processo con durata minore. Non c'è pre-emption.
 
 Il programma deve stampare a video la lista degli ID di tutti i processi nell'ordine in cui verranno eseguiti.
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// Struttura per memorizzare i dati di un singolo processo
+struct Processo 
+{
+    int id;
+    int arrivo;
+    int durata;
+    int priorita;
+};
+
+void schedulaProcessiES1(vector<Processo> vettProcessi) 
+{
+    //SCRIVI QUI IL CODICE PER L'ESERCIZIO 1
+
+    
+}
+
+void schedulaProcessiES2(vector<Processo> vettProcessi) 
+{   
+    //SCRIVI QUI IL CODICE PER L'ESERCIZIO 2
+    
+    
+}
+
+int main()
+{
+    ifstream file("input.txt");
+    
+    // Controllo se il file esiste e si apre correttamente
+    if (!file.is_open()) {
+        cout << "Errore nell'apertura del file!" << endl;
+        return 1;
+    }
+
+    int n;
+    file >> n; // Legge il numero totale di processi dalla prima riga
+
+    vector<Processo> vettProcessi(n);
+
+    // Popola il vettore di processi
+    for (int i = 0; i < n; i++) 
+    {
+        file >> vettProcessi[i].id 
+             >> vettProcessi[i].arrivo 
+             >> vettProcessi[i].durata 
+             >> vettProcessi[i].priorita;
+    }
+    
+    file.close();
+
+    //Test esercizio 1
+    schedulaProcessiES1(vettProcessi);
+    
+    //Test esercizio 2
+    schedulaProcessiES2(vettProcessi);
+    return 0;
+}
 */
 #include <iostream>
 #include <fstream>
